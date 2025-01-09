db1e6b994ee48b480f9552df2b1fb54274f36bd2
aananev <6167006@bk.ru>
2025-01-09 11:56:19 +0300

    Initial commit

---
+++ hazy-admin/src/types/categories.ts
@@ -0,0 +1,11 @@
+export const generateCategories = (): Category[] => [
+    {id:0, name: 'Футболки'},
+    {id:1, name: 'Лщнг-сливы'},
+    {id:2, name: 'Худи'},
+ ]
+
+
+export type Category = {
+    id: number;
+    name: string;
+}
---
+++ hazy-admin/src/types/products.ts
@@ -0,0 +1,15 @@
+
+const productReferences = {
+    tShirts: [
+        'MamMamMam',
+        'MamMamMam2',
+        'MamMamMam3',
+        'MamMamMam4',
+    ],
+    longSLives: [
+        'MamMamMamLong',
+        'MamMamMamLong2',
+        'MamMamMamLong3',
+        'MamMamMamLong4',
+    ],
+}
