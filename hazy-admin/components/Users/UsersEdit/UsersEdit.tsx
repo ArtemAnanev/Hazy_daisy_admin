b8a6be2ae7fd8cd0c6d95356a18e899ca07cde93
aananev <6167006@bk.ru>
2025-01-14 12:28:52 +0300

    изменение пароля пользователя

--- hazy-admin/components/Users/UsersEdit/UsersEdit.tsx
+++ hazy-admin/components/Users/UsersEdit/UsersEdit.tsx
@@ -1,16 +1,66 @@
-import {Edit, PasswordInput, required} from 'react-admin';
+import {Button, Edit, useGetOne, useGetRecordId, useNotify} from 'react-admin';
 import {UsersForm} from "../UsersForm/UsersForm";
+import {USERS_SOURCE_NAME} from '../../../constants/sourceNames';
+import {useCallback, useMemo, useState} from 'react';
+import api from '../../../api/apiInstance'
 
-export const UsersEdit = () => (
-    <Edit>
-        <UsersForm
-            passwordComponent={
-            <></>
-                // <PasswordInput
-                //     source='password'
-                //     validate={[required()]}
-                //     resettable/>
-            }
-        />
-    </Edit>
-)
+export const UsersEdit = () => {
+    const notify = useNotify();
+    const id = useGetRecordId()
+    const user = useGetOne(USERS_SOURCE_NAME, {id})
+
+    const [spinner, setSpinner] = useState(false)
+    const [passwordRestoreInitialized, setPasswordRestoreInitialized] = useState(false)
+
+    const handleInitializePasswordRestore = useCallback(async () => {
+        try {
+            setSpinner(true)
+
+            await api.post('/admin/password-restore', {
+                email: user.data.email,
+            })
+
+            notify('Инициализация прошла успешно!', {type: 'success'})
+
+            setPasswordRestoreInitialized(true)
+        } catch (error) {
+            notify(`Произошла ошибка: ${(error as Error).message}`, {type: 'error'})
+
+            setPasswordRestoreInitialized(false)
+        } finally {
+            setSpinner(false)
+        }
+    }, [notify, user.data?.email])
+
+    const renderPasswordRestoreContent = useMemo(() => {
+        if (passwordRestoreInitialized) {
+            return (
+                <p>
+                    На почту пользователя {user.data?.email} отправленно письмо с ссылкой
+                    для сброса пароля
+                </p>
+            )
+        }
+
+        return (
+            <Button
+                size='large'
+                onClick={handleInitializePasswordRestore}
+                disabled={spinner}
+            >
+                {spinner ? <>Инициализация</> : <>ИнициироватЬ сброс пароля</>}
+            </Button>
+        )
+    }, [
+        handleInitializePasswordRestore,
+        passwordRestoreInitialized,
+        spinner,
+        user.data?.email,
+    ])
+
+    return (
+        <Edit>
+            <UsersForm passwordComponent={renderPasswordRestoreContent}/>
+        </Edit>
+    )
+}
